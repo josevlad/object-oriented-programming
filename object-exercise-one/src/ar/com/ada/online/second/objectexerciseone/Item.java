013876894294fb9e318d35c79f44cbbc24ebd730
Vladimir Castañeda <vladimir29castaneda@gmail.com>
2020-08-26 21:10:23 -0300

    finish object-exercise-two

--- object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Item.java
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Item.java
@@ -3,27 +3,36 @@ package ar.com.ada.online.second.objectexerciseone;
 import java.util.Date;
 
 public class Item {
+
     String name;
     Double price;
     Double tax;
     Date expirationDate;
 
+    Double getTotalToPay() {
+        Double result = this.price + this.tax;
+        return result;
+        // return this.price + this.tax;
+    }
+
     void calculateTax() {
         this.tax = this.price * 0.12;
     }
 
-    Double getTotalToPay() {
-        Double totalToPay = this.price + this.tax;
-        return totalToPay;
-    }
+//    Boolean canItBeSold() {
+//        Date today = new Date();
+//        Boolean result = expirationDate.after(today);
+//        return result;
+//    }
 
     Boolean canItBeSold() {
         Date today = new Date();
         Boolean canItBeSold = true;
-        
-        if (expirationDate.compareTo(today) < 0)
+
+        if (today.after(this.expirationDate)) {
             canItBeSold = false;
-        
+        }
+
         return canItBeSold;
     }
 }
--- object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Main.java
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Main.java
@@ -1,39 +1,38 @@
 package ar.com.ada.online.second.objectexerciseone;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        // esta instancia es para convertir un string de fecha a un objeto Date
+    public static void main(String[] args) throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         Scanner keyboard = new Scanner(System.in);
-
-        // Creo una instancia (o un objeto) de la clase Item
         Item item = new Item();
 
-        // Pido los datos y los voy seteando a los atributos del objeto
         System.out.print("Ingrese el nombre del Item: ");
-        item.name = keyboard.nextLine();
+        item.name = keyboard.nextLine(); // next();
 
         System.out.print("Ingrese el precio del Item: ");
-        item.price = Double.valueOf(keyboard.nextLine());
+//        item.price = keyboard.nextDouble();
+        item.price = Double.valueOf(keyboard.next());
 
-        System.out.print("Ingrese la fecha de vencimiento del Item (dia-mes-año): ");
-        item.expirationDate = dateFormat.parse(keyboard.nextLine());
+        System.out.print("Ingrese la fecha de vencimiento del Item (dd-MM-yyyy): ");
+        String date = keyboard.next();
+        item.expirationDate = dateFormat.parse(date);
+//        item.expirationDate = dateFormat.parse(keyboard.next());
 
-        // ejecuto el metodo para calcular el impuesto
         item.calculateTax();
 
-        // verifico si se puede vender el item
         if (item.canItBeSold()) {
+            Double totalToPay = item.getTotalToPay();
             System.out.printf(
-                    "\nNombre: %s\nPrecio %.2f\nImpuesto: %.2f\nTotal a pagar: %.2f",
-                    item.name,
-                    item.price,
-                    item.tax,
-                    item.getTotalToPay()
+                "- Nombre: %s\n- Precio: %.2f\n- Impuesto: %.2f\n- Total a pagar: %.2f",
+                item.name,
+                item.price,
+                item.tax,
+                totalToPay // or item.getTotalToPay()
             );
         } else {
             System.out.println("Este Ítem no está habilitado para la venta");
